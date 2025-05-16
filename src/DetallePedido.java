ae8b08f363d47c4aad0f991ab2067a81312e8e78
Luciano Molaro <molarolucianodelongo@gmail.com>
2025-05-16 01:58:04 -0300

    lucho parte

---
+++ src/DetallePedido.java
@@ -0,0 +1,48 @@
+public class DetallePedido {
+    private Integer cantidad;
+    private Double subtotal;
+
+    private Articulo articulo;
+    private Pedido pedido;
+
+    public DetallePedido() {
+    }
+
+    public DetallePedido(Integer cantidad, Articulo articulo) {
+        this.cantidad = cantidad;
+        this.articulo=articulo;
+        this.subtotal= cantidad * articulo.getPrecioVenta();
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+}
