cbaf27522eb57012af8c45bc392ba650e0575e4d
EmiG4rcia <emi.almagro.2001@gmail.com>
2025-05-20 18:32:36 -0300

    Instanciación de las clases restantes y modificacion de las clases Articulo, ArticuloManufacturadoDetalle e ImagenPromocion

--- src/Principal.java
+++ src/Principal.java
@@ -14,40 +14,99 @@ public class Principal {
         loc1.setProvincia(prov1);
         loc2.setProvincia(prov2);
 
-        Domicilio dom1 = new Domicilio("Avenida Belgrano",1200,5501);
-        Domicilio dom2 = new Domicilio("Avenida Libertadores",500,1499);
+        Domicilio dom1 = new Domicilio("Avenida Belgrano", 1200, 5501);
+        Domicilio dom2 = new Domicilio("Avenida Libertadores", 500, 1499);
         dom1.setLocalidad(loc1);
         dom2.setLocalidad(loc2);
 
-        Sucursal suc1 = new Sucursal("Sucursal Mendoza", LocalTime.of(8,30),LocalTime.of(22,30));
-        Sucursal suc2 = new Sucursal("Sucursal Buenos Aires",LocalTime.of(9,0),LocalTime.of(23,0));
+        Sucursal suc1 = new Sucursal("Sucursal Mendoza", LocalTime.of(8, 30), LocalTime.of(22, 30));
+        Sucursal suc2 = new Sucursal("Sucursal Buenos Aires", LocalTime.of(9, 0), LocalTime.of(23, 0));
         suc1.setDomicilio(dom1);
         suc2.setDomicilio(dom2);
 
-        Empresa empresa = new Empresa("Smash Burgers","Smash Burgers S.A.",332084567);
+        Empresa empresa = new Empresa("Smash Burgers", "Smash Burgers S.A.", 332084567);
         empresa.addSucursal(suc1);
         empresa.addSucursal(suc2);
         System.out.println(empresa);
 
-        Factura factura = new Factura(20000.00,FormaPago.MERCADO_PAGO, LocalDate.now());
-        Factura factura2 = new Factura(10000.00,FormaPago.EFECTIVO, LocalDate.now());
+        Factura factura = new Factura(20000.00, FormaPago.MERCADO_PAGO, LocalDate.now());
+        Factura factura2 = new Factura(10000.00, FormaPago.EFECTIVO, LocalDate.now());
 
         //Instanciamos el pedido
-        Pedido pedido = new Pedido(LocalTime.of(10,30),34000.00,1000.00,Estado.PENDIENTE, TipoEnvio.DELIVERY,FormaPago.EFECTIVO,LocalDate.now());
+        Pedido pedido = new Pedido(LocalTime.of(10, 30), 34000.00, 1000.00, Estado.PENDIENTE, TipoEnvio.DELIVERY, FormaPago.EFECTIVO, LocalDate.now());
         //Instanciamos el detalle pedido
         DetallePedido detalle = new DetallePedido(2);
-        //Instanciamos el artículo
+        //Agregamos el detalle al pedido
+        pedido.addDetallePedido(detalle);
+        //Agregamos domicilio al pedido
+        pedido.setDomicilio(dom1);
+        //Agregamos la sucursal al pedido
+        pedido.setSucursal(suc1);
+
+        //Instanciamos los artículos
+        ArticuloManufacturado articuloManufacturado1 = new ArticuloManufacturado("Hamburguesa", "1500", 1000.00, true, "Hamburguesa con queso, tomate y lechuga", 18, "Coccion media");
+        //Instanciamos los detalles de los articulos manufacturados
+        ArticuloManufacturadoDetalle detalleArticuloManufacturado1 = new ArticuloManufacturadoDetalle(1);
+        ArticuloManufacturadoDetalle detalleArticuloManufacturado2 = new ArticuloManufacturadoDetalle(2);
+        //Instanciamos ArticuloInsumo
+        ArticuloInsumo articuloInsumo1 = new ArticuloInsumo("Queso", "100", 100.00, true, 7000.00, 15, 5, 25, false);
+        detalleArticuloManufacturado1.setArticuloInsumo(articuloInsumo1);
+        articuloManufacturado1.addDetalle(detalleArticuloManufacturado1);
+        articuloManufacturado1.addDetalle(detalleArticuloManufacturado2);
+
+        //Instanciamos imagenes para articulo
+        ImagenArticulo imgArticulo1 = new ImagenArticulo("Fachada del Restaurante", "imagen.jpg");
+        ImagenArticulo imgArticulo2 = new ImagenArticulo("Cocina", "imagen.jpg");
+        //Agregamos imagenes a articulo
+        articuloManufacturado1.addImagen(imgArticulo1);
+        articuloManufacturado1.addImagen(imgArticulo2);
+
+        //Instanciamos unidad de medida
+        UnidadMedida unidadMedida = new UnidadMedida("gramos (g)");
+
+        //Agregamos unidades de medida a articulo
+        articuloManufacturado1.setUnidadMedida(unidadMedida);
+
+        //Instanciamos categoria
+        Categoria categoria1 = new Categoria("Hamburguesas");
+        Categoria categoria2 = new Categoria("Bebidas");
+
+        //Agregamos categorias a Sucursal
+        suc1.addCategoria(categoria1);
+        suc1.addCategoria(categoria2);
+
+        //Agregamos articulo a categoria
+        categoria1.addArticulo(articuloManufacturado1);
+        //Agregamos articulo a DetallePedido
+        detalle.setArticulo(articuloManufacturado1);
 
         //Instanciamos cliente con su usuario e imagen
-        Usuario user = new Usuario("227","mfernandez");
+        Usuario user = new Usuario("227", "mfernandez");
         ImagenCliente imgCliente = new ImagenCliente("imagen.jpg");
-        Cliente cliente = new Cliente("Mariana","Fernandez","2611234567","mfernandez@uc.cl",LocalDate.of(2000, 10, 10));
+        Cliente cliente = new Cliente("Mariana", "Fernandez", "2611234567", "mfernandez@uc.cl", LocalDate.of(2000, 10, 10));
         cliente.setImagen(imgCliente);
         cliente.setUsuario(user);
         //Istanciamos el domicilio del cliente y se lo agregamos a la lista de domicilios
-        Domicilio dom3 = new Domicilio("Avenida Lavalle",1500,5601);
+        Domicilio dom3 = new Domicilio("Avenida Lavalle", 1500, 5601);
         cliente.addDomicilio(dom3);
         //Agregamos pedido a cliente
         cliente.addPedido(pedido);
+
+        //Instanciamos promocion
+        Promocion promocion1 = new Promocion("Promo 10%", LocalDate.MIN, LocalDate.MAX, LocalTime.MIN, LocalTime.MAX, "Descuento por el aniversario del restaurante", 7500.00, TipoPromocion.PROMOCION);
+
+        //Agregamos articulo a promocion
+        promocion1.addArticulo(articuloManufacturado1);
+
+        //Instanciamos ImagenPromocion
+        ImagenPromocion imagenPromocion = new ImagenPromocion("Imagen de platillo clásico de la casa","imagen.jpg");
+
+        //Agregamos ImagenPromocion a promocion
+        promocion1.addImagen(imagenPromocion);
+
+        //Agregamos Promocion a Sucursal
+        suc1.addPromocion(promocion1);
+
+        
     }
 }
